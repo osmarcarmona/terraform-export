7e469652f4241c75ea2d4b0417331fe57056f7b0
cmosmar <imposible_no@hotmail.com>
2019-05-24 12:29:32 -0500

    Cluster-web-aws

---
+++ vars.tf
@@ -0,0 +1,21 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# ENVIRONMENT VARIABLES
+# Define these secrets as environment variables
+# ---------------------------------------------------------------------------------------------------------------------
+
+# AWS_ACCESS_KEY_ID
+# AWS_SECRET_ACCESS_KEY
+
+# ---------------------------------------------------------------------------------------------------------------------
+# OPTIONAL PARAMETERS
+# ---------------------------------------------------------------------------------------------------------------------
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  default = 8080
+}
+
+variable "ssh_port" {
+  description = "SSH port connection"
+  default = 22
+}
